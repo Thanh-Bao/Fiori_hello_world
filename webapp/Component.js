3901b342ae8d0e53e062b00c0ca653f8ccf51c3b
Bảo Bảo <thanh.bao@outlook.com>
2023-04-07 19:03:58 +0700

    Week 1 Unit 4: Creating a Configurable App Component
    View xml bị lặp 2 lần

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,7 +1,31 @@
-sap.ui.getCore().attachInit(() => {
-    sap.ui.xmlview({
-        viewName: "opensap.myapp.view.App"
+// sap.ui.getCore().attachInit(() => {
+//     sap.ui.xmlview({
+//         viewName: "opensap.myapp.view.App"
+//     }).placeAt("content");
+// })
+
+sap.ui.getCore().attachInit(function () {
+    new sap.ui.core.ComponentContainer({
+        name: "opensap.myapp"
     }).placeAt("content");
-})
+});
 
+sap.ui.define([
+    "sap/ui/core/UIComponent"
+], function (UIComponent) {
+    "use strict";
 
+    return UIComponent.extend("opensap.myapp.Component", {
+
+        metadata: {
+            manifest: "json"
+        },
+
+        init: function () {
+            // call the init function of the parent
+            UIComponent.prototype.init.apply(this, arguments);
+            // additional initialization can be done here
+        }
+
+    });
+});
