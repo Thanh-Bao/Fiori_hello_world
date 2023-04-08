79a92adb52077fb1469d0c0f127ee02540ec2c4f
Bảo Bảo <thanh.bao@outlook.com>
2023-04-08 10:14:21 +0700

    Week 1 Unit 6 - Exercise: Containers and Layout

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -9,12 +9,8 @@ sap.ui.define([
 			var oBundle = this.getView().getModel("i18n").getResourceBundle();
 			var sRecipient = this.getView().getModel("helloPanel").getProperty("/recipient/name");
 			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
-
 			// show message
 			MessageToast.show(sMsg);
-
-
 		}
-
 	});
 });
