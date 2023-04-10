fd2823d6d91497826a08b5d3a832f33bdbac8997
Bảo Bảo <thanh.bao@outlook.com>
2023-04-10 21:32:17 +0700

    Week 2 Unit 5,6 WEEK 2 UNIT 6 WORKING WITH ODATA SERVICES

--- webapp/Component.js
+++ webapp/Component.js
@@ -27,6 +27,10 @@ sap.ui.define(['sap/ui/core/UIComponent'], function (UIComponent) {
 		init: function () {
 			// call the init function of the parent
 			UIComponent.prototype.init.apply(this, arguments);
+
+			// used only for this lessons to show the request individually...
+			this.getModel().setUseBatch(false);
+
 			// additional initialization can be done here
 		},
 	});
--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -16,6 +16,15 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
 			// show message
 			MessageToast.show(sMsg);
 		},
+		onItemSelected: function (oEvent) {
+			var oSelectedItem = oEvent.getSource();
+			var oContext = oSelectedItem.getBindingContext();
+			var sPath = oContext.getPath();
+			var oProductDetailPanel = this.byId("productDetailsPanel");
+
+			oProductDetailPanel.bindElement({ path: sPath });
+			this.byId("productDetailsPanel").setVisible(true);
+		},
 		onFilterProducts: function (oEvent) {
 
 			// build filter array
