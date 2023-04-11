11ede09045de4fc8e70ef1b24c8e27ca904164e7
Bảo Bảo <thanh.bao@outlook.com>
2023-04-11 13:34:34 +0700

    init template

--- webapp/Component.js
+++ webapp/Component.js
@@ -1,37 +1,15 @@
-sap.ui.getCore().attachInit(function () {
-	// workaround begin: start a mock server
-	jQuery.sap.require('sap.ui.core.util.MockServer');
-	var oMockServer = new sap.ui.core.util.MockServer({
-		rootUri: '/destinations/ES5/sap/opu/odata/IWBEP/GWSAMPLE_BASIC/',
-	});
-	oMockServer.simulate('metadata.xml', {
-		sMockdataBaseUrl: 'mockdata',
-		bGenerateMissingMockData: true,
-	});
-	oMockServer.start();
-	// workaround end
 
-	new sap.ui.core.ComponentContainer({
-		name: 'opensap.myapp',
-	}).placeAt('content');
-});
+sap.ui.define([
+	"sap/m/Button",
+	"sap/m/MessageToast"
+], function (Button, MessageToast) {
+	"use strict";
 
-sap.ui.define(['sap/ui/core/UIComponent'], function (UIComponent) {
-	'use strict';
+	new Button({
+		text: "Ready...",
+		press: function () {
+			MessageToast.show("Hello World!");
+		}
+	}).placeAt("content");
 
-	return UIComponent.extend('opensap.myapp.Component', {
-		metadata: {
-			manifest: 'json',
-		},
-
-		init: function () {
-			// call the init function of the parent
-			UIComponent.prototype.init.apply(this, arguments);
-
-			// used only for this lessons to show the request individually...
-			this.getModel().setUseBatch(false);
-
-			// additional initialization can be done here
-		},
-	});
 });
