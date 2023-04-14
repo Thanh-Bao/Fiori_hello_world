b7bd20873b256797d87820f6b855242ad2d10ccb
Bảo Bảo <thanh.bao@outlook.com>
2023-04-14 10:59:35 +0700

    Step 1: No Data Binding

--- webapp/index.js
+++ webapp/index.js
@@ -1,13 +1,11 @@
-sap.ui.define([
-    "sap/ui/core/ComponentContainer"
-], function (ComponentContainer) {
+sap.ui.require([
+    "sap/m/Text"
+], function (Text) {
     "use strict";
 
-    new ComponentContainer({
-        name: "sap.ui.demo.walkthrough",
-        settings: {
-            id: "walkthrough"
-        },
-        async: true
-    }).placeAt("content");
+    // Attach an anonymous function to the SAPUI5 'init' event
+    sap.ui.getCore().attachInit(function () {
+        // Create a text UI element that displays a hardcoded text string
+        new Text({ text: "Hello world" }).placeAt("content");
+    });
 });
